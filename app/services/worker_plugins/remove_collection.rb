c7178475d3d44b2116ad39058dfe39bc2714a1bc
kaspernj <kaspernj@gmail.com>
2020-03-03 18:06:07 +0100

    Fixed complexity offences and return only ID's since no more than one class can be included

--- app/services/worker_plugins/remove_collection.rb
+++ app/services/worker_plugins/remove_collection.rb
@@ -4,7 +4,6 @@ class WorkerPlugins::RemoveCollection < WorkerPlugins::ApplicationService
   def initialize(query:, workplace:)
     @query = query
     @workplace = workplace
-    @destroyed = {}
   end
 
   def execute
@@ -12,43 +11,9 @@ class WorkerPlugins::RemoveCollection < WorkerPlugins::ApplicationService
     succeed!(destroyed: destroyed, mode: :destroyed)
   end
 
-  def add_query_to_workplace
-    created[model_class.name] ||= []
-    primary_key = resources_to_add.klass.primary_key
-    created[model_class.name] += resources_to_add.pluck(primary_key.to_sym)
-
-    select_sql = resources_to_add.select("
-      NOW(),
-      '#{resources_to_add.klass.name}',
-      \"#{resources_to_add.klass.table_name}\".\"#{primary_key}\",
-      NOW(),
-      '#{workplace.id}'
-    ")
-
-    sql = "
-      INSERT INTO
-        worker_plugins_workplace_links
-
-      (
-        created_at,
-        resource_type,
-        resource_id,
-        updated_at,
-        workplace_id
-      )
-
-      #{select_sql.to_sql}
-    "
-
-    WorkerPlugins::WorkplaceLink.connection.execute(sql)
-  end
-
   def remove_query_from_workplace
     links_query = workplace.workplace_links.where(resource_type: model_class.name, resource_id: query.select(:id))
-
-    destroyed[model_class.name] ||= []
-    destroyed[model_class.name] += links_query.pluck(:resource_id)
-
+    @destroyed = links_query.pluck(:resource_id)
     links_query.delete_all
   end
 
--- spec/services/worker_plugins/add_collection_spec.rb
+++ spec/services/worker_plugins/add_collection_spec.rb
@@ -17,7 +17,7 @@ describe WorkerPlugins::AddCollection do
       expect { result }
         .to change(WorkerPlugins::WorkplaceLink, :count).by(2)
 
-      expect(result.fetch(:created)).to eq("Task" => [task1.id, task2.id])
+      expect(result.fetch(:created)).to eq [task1.id, task2.id]
     end
   end
 
