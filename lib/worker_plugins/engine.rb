6693b905c46fe388d56afaa18e6f750b12f9db32
kaspernj <kaspernj@gmail.com>
2021-01-06 20:14:20 +0100

    Dont auto append migrations any more

--- lib/worker_plugins/engine.rb
+++ lib/worker_plugins/engine.rb
@@ -6,16 +6,4 @@ class WorkerPlugins::Engine < ::Rails::Engine
   # Add translations to load path.
   path = File.realpath(File.join(File.dirname(__FILE__), "..", "..", "config", "locales"))
   I18n.load_path += Dir[File.join(path, "**", "*.{rb,yml}")]
-
-  initializer :append_migrations do |app|
-    unless app.root.to_s.match root.to_s
-      if Rails.version.start_with?("3")
-        app.config.paths["db/migrate"] += config.paths["db/migrate"].expanded
-      else
-        config.paths["db/migrate"].expanded.each do |another_path|
-          app.config.paths["db/migrate"] << another_path
-        end
-      end
-    end
-  end
 end
