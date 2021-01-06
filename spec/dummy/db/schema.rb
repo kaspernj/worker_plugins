4d62445ab0e096e31a11c76088344bc980a00322
Kasper Stöckel <kaspernj@gmail.com>
2021-01-06 20:22:02 +0100

    Change links resource ID to string to support UUID (#71)
    * Change links resource ID to string to support UUID
    
    * Removed old migrations again
    
    * Dont auto append migrations any more

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -2,19 +2,19 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_06_090637) do
+ActiveRecord::Schema.define(version: 2021_01_06_190518) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name", null: false
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_tasks_on_user_id"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2019_11_06_090637) do
   create_table "worker_plugins_workplace_links", force: :cascade do |t|
     t.integer "workplace_id", null: false
     t.string "resource_type", null: false
-    t.integer "resource_id", null: false
+    t.string "resource_id", null: false
     t.json "custom_data"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(version: 2019_11_06_090637) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["active"], name: "index_worker_plugins_workplaces_on_active"
-    t.index ["user_type", "user_id"], name: "index_worker_plugins_workplaces_on_user_type_and_user_id"
+    t.index ["user_type", "user_id"], name: "index_worker_plugins_workplaces_on_user"
   end
 
   add_foreign_key "tasks", "users"
