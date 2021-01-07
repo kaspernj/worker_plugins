cfaa4a5e2bd210a8f34720c9f47b85782df3fa05
Kasper Stöckel <kaspernj@gmail.com>
2021-01-07 09:41:34 +0100

    Auto cast type if not same (#72)
    * Auto cast type if not same
    
    * Fixed migrations
    
    * Removed gem migrations
    
    * Fixed complexity offences

---
+++ app/services/worker_plugins/select_column_with_type_cast.rb
@@ -0,0 +1,41 @@
+class WorkerPlugins::SelectColumnWithTypeCast < WorkerPlugins::ApplicationService
+  attr_reader :column_name_to_select, :column_to_compare_with, :query
+
+  def initialize(column_name_to_select:, column_to_compare_with:, query:)
+    @column_name_to_select = column_name_to_select
+    @column_to_compare_with = column_to_compare_with
+    @query = query
+  end
+
+  def execute
+    return succeed! query.select(column_name_to_select) if same_type?
+
+    if column_to_compare_with.type == :string
+      succeed! query_with_varchar
+    elsif column_to_compare_with.type == :integer
+      succeed! query_with_integer
+    else
+      raise "Unknown type: #{column_to_compare_with.type}"
+    end
+  end
+
+  def column_to_select
+    @column_to_select ||= model_class.column_for_attribute(column_name_to_select)
+  end
+
+  def model_class
+    @model_class ||= query.klass
+  end
+
+  def query_with_integer
+    query.select("CAST(#{model_class.table_name}.#{column_name_to_select} AS BIGINT)")
+  end
+
+  def query_with_varchar
+    query.select("CAST(#{model_class.table_name}.#{column_name_to_select} AS VARCHAR)")
+  end
+
+  def same_type?
+    column_to_select.type == column_to_compare_with.type
+  end
+end
--- app/services/worker_plugins/switch_collection.rb
+++ app/services/worker_plugins/switch_collection.rb
@@ -22,15 +22,30 @@ class WorkerPlugins::SwitchCollection < WorkerPlugins::ApplicationService
     end
   end
 
-  def ids_added_already
+  def ids_added_already_query
     workplace
       .workplace_links
-      .where(resource_type: model_class.name, resource_id: query.select(:id))
-      .select(:resource_id)
+      .where(resource_type: model_class.name, resource_id: query_with_selected_ids)
+  end
+
+  def ids_added_already
+    WorkerPlugins::SelectColumnWithTypeCast.execute!(
+      column_name_to_select: :resource_id,
+      column_to_compare_with: model_class.column_for_attribute(:id),
+      query: ids_added_already_query
+    )
   end
 
   def model_class
     @model_class ||= query.klass
+  end
+
+  def query_with_selected_ids
+    WorkerPlugins::SelectColumnWithTypeCast.execute!(
+      column_name_to_select: :id,
+      column_to_compare_with: WorkerPlugins::WorkplaceLink.column_for_attribute(:resource_id),
+      query: query
+    )
   end
 
   def resources_to_add
