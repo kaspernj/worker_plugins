a88c537d144615d13533646cd89d5143804d1dd5
kaspernj <kaspernj@gmail.com>
2021-01-07 09:34:41 +0100

    Removed gem migrations

--- spec/dummy/db/migrate/20210107082835_change_workplace_links_resource_id_to_string_to_support_uuids.worker_plugins.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from worker_plugins (originally 20200702072306)
-class ChangeWorkplaceLinksResourceIdToStringToSupportUuids < ActiveRecord::Migration[6.0]
-  def change
-    change_column :worker_plugins_workplace_links, :resource_id, :string
-  end
-end
--- spec/dummy/db/migrate/20210107082836_change_resource_id_to_string_to_support_uuid.worker_plugins.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from worker_plugins (originally 20210106190349)
-class ChangeResourceIdToStringToSupportUuid < ActiveRecord::Migration[6.1]
-  def change
-    change_column :worker_plugins_workplace_links, :resource_id, :string
-  end
-end
