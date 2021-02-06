4db1342d5f63dac8619fef10f2e46b4bdaee9243
kaspernj <kaspernj@gmail.com>
2021-02-06 10:27:19 +0100

    Fixed wrong created_at and updated_at by using Rails's time instead of DB's

--- app/services/worker_plugins/application_service.rb
+++ app/services/worker_plugins/application_service.rb
@@ -1,10 +1,22 @@
 class WorkerPlugins::ApplicationService < ServicePattern::Service
-  def db_now_method
-    if sqlite?
-      "TIME('now')"
-    else
-      "NOW()"
+  def db_now_value
+    @db_now_value ||= begin
+      time_string = Time.zone.now.strftime("%Y-%m-%d %H:%M:%S")
+
+      if postgres?
+        "CAST(#{quote(time_string)} AS TIMESTAMP)"
+      else
+        quote(time_string)
+      end
     end
+  end
+
+  def quote(value)
+    WorkerPlugins::Workplace.connection.quote(value)
+  end
+
+  def postgres?
+    ActiveRecord::Base.connection.instance_values["config"][:adapter].downcase.include?("postgres")
   end
 
   def sqlite?
