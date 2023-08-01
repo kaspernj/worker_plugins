fa17406070315c2e19914201ba63f0214121d888
kaspernj <k@spernj.org>
2023-08-01 10:23:47 +0000

    Fixed offence

--- app/models/worker_plugins/workplace.rb
+++ app/models/worker_plugins/workplace.rb
@@ -1,4 +1,4 @@
-class WorkerPlugins::Workplace < WorkerPlugins::ApplicationRecord # rubocop:disable Rails/ApplicationRecord
+class WorkerPlugins::Workplace < WorkerPlugins::ApplicationRecord
   self.table_name = "worker_plugins_workplaces"
 
   has_many :workplace_links, dependent: :destroy
