ef95acad37b0cd2e6d154d8bb29e3a5016438205
kaspernj <k@spernj.org>
2023-12-27 17:50:30 +0000

    Fixed offences

--- app/services/worker_plugins/user_relationship_polymorphic.rb
+++ app/services/worker_plugins/user_relationship_polymorphic.rb
@@ -2,11 +2,9 @@ class WorkerPlugins::UserRelationshipPolymorphic
   def self.execute!
     WorkerPlugins::Workplace.columns_hash.key?("user_type")
   rescue ActiveRecord::StatementInvalid => e
-    if e.message.start_with?("Could not find table")
-      # Fall back to true if we are in the middle of a migration or something
-      return true
-    else
-      raise e
-    end
+    # Fall back to true if we are in the middle of a migration or something
+    return true if e.message.start_with?("Could not find table")
+
+    raise e
   end
 end
