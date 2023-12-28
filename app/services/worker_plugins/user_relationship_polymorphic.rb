024a2504cf89d093df7e7aabe1cf008f04d5b5ce
kaspernj <k@spernj.org>
2023-12-28 10:25:47 +0000

    Fixed alternative error message

--- app/services/worker_plugins/user_relationship_polymorphic.rb
+++ app/services/worker_plugins/user_relationship_polymorphic.rb
@@ -1,9 +1,9 @@
 class WorkerPlugins::UserRelationshipPolymorphic
   def self.execute!
     WorkerPlugins::Workplace.columns_hash.key?("user_type")
-  rescue ActiveRecord::StatementInvalid => e
+  rescue => e
     # Fall back to true if we are in the middle of a migration or something
-    return true if e.message.start_with?("Could not find table")
+    return true if e.message.start_with?("Could not find table") || e.message.match(/Table '(.+)' doesn't exist/)
 
     raise e
   end
