a602401c29be2c0b7fabaf64669e53d7bfcdae4d
kaspernj <kasper@diestoeckels.de>
2026-03-23 08:29:11 +0000

    Handle UUID workplace link casts

--- app/services/worker_plugins/select_column_with_type_cast.rb
+++ app/services/worker_plugins/select_column_with_type_cast.rb
@@ -4,14 +4,10 @@ class WorkerPlugins::SelectColumnWithTypeCast < WorkerPlugins::ApplicationServic
   def perform
     return succeed! query.select(column_name_to_select) if same_type?
 
-    if column_to_compare_with.type == :string
-      succeed! query_with_varchar
-    elsif column_to_compare_with.type == :integer
+    if column_to_compare_with.type == :integer
       succeed! query_with_integer
     else
-      raise "Cant handle type cast between types: " \
-        "#{model_class.table_name}.#{column_name_to_select} (#{column_to_select.type}) " \
-        "#{column_to_compare_with.name} (#{column_to_compare_with.type})"
+      succeed! query_with_varchar
     end
   end
 
---
+++ spec/services/worker_plugins/select_column_with_type_cast_spec.rb
@@ -0,0 +1,37 @@
+require "rails_helper"
+
+describe WorkerPlugins::SelectColumnWithTypeCast do
+  describe "#execute!" do
+    it "casts to varchar for non-integer compare columns" do
+      compare_column = instance_double(
+        ActiveRecord::ConnectionAdapters::Column,
+        name: "id",
+        type: nil
+      )
+
+      result = described_class.execute!(
+        column_name_to_select: :id,
+        column_to_compare_with: compare_column,
+        query: Task.all
+      )
+
+      expect(result.to_sql).to include("CAST(tasks.id AS VARCHAR)")
+    end
+
+    it "casts to bigint for integer compare columns" do
+      compare_column = instance_double(
+        ActiveRecord::ConnectionAdapters::Column,
+        name: "id",
+        type: :integer
+      )
+
+      result = described_class.execute!(
+        column_name_to_select: :resource_id,
+        column_to_compare_with: compare_column,
+        query: WorkerPlugins::WorkplaceLink.all
+      )
+
+      expect(result.to_sql).to include("CAST(worker_plugins_workplace_links.resource_id AS BIGINT)")
+    end
+  end
+end
