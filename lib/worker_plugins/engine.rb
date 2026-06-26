9b33a9991176f9913aed250737d026c0d798a322
kaspernj <kaspernj@users.noreply.github.com>
2026-06-26 19:09:16 +0000

    Fix RuboCop one class per file offense

--- lib/worker_plugins/engine.rb
+++ lib/worker_plugins/engine.rb
@@ -1,9 +1,9 @@
-module WorkerPlugins; end
+module WorkerPlugins
+  class Engine < Rails::Engine
+    isolate_namespace WorkerPlugins
 
-class WorkerPlugins::Engine < Rails::Engine
-  isolate_namespace WorkerPlugins
-
-  # Add translations to load path.
-  path = File.realpath(File.join(File.dirname(__FILE__), "..", "..", "config", "locales"))
-  I18n.load_path += Dir[File.join(path, "**", "*.{rb,yml}")]
+    # Add translations to load path.
+    path = File.realpath(File.join(File.dirname(__FILE__), "..", "..", "config", "locales"))
+    I18n.load_path += Dir[File.join(path, "**", "*.{rb,yml}")]
+  end
 end
